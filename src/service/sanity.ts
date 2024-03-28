6163fcb7e85348e747c07bd1ddc4640d08c9eac7
ohs8283 <ohs66428283@gmail.com>
2024-03-28 23:54:27 +0900

    feat: 로그인시 sanity 데이터베이스 사용자 추가

---
+++ src/service/sanity.ts
@@ -0,0 +1,8 @@
+import { createClient } from "@sanity/client";
+
+export const client = createClient({
+  projectId: process.env.SANITY_PROJECT_ID,
+  dataset: process.env.SANITY_DATASET,
+  useCdn: false,
+  token: process.env.SANITY_SECRET_TOKEN,
+});
