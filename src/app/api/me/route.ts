2a176402bb9d73e257057d1b18fac69b19bc5231
ohs8283 <ohs66428283@gmail.com>
2024-03-29 03:34:30 +0900

    feat: SWR 적용

---
+++ src/app/api/me/route.ts
@@ -0,0 +1,16 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/route";
+import { NextResponse } from "next/server";
+import { getUserByUsername } from "@/service/user";
+
+export async function GET(request: Request) {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  if (!user) {
+    return new Response("Authentication Error", { status: 401 });
+  }
+  return getUserByUsername(user.username as string).then((data) =>
+    NextResponse.json(data)
+  );
+}
