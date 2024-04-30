99b788a46f9424aa58e73716fb68800c3a9cfdb6
ohs8283 <ohs66428283@gmail.com>
2024-04-30 18:02:45 +0900

    feat: 댓글 삭제 기능 추가

--- src/app/api/comments/route.ts
+++ src/app/api/comments/route.ts
@@ -1,4 +1,4 @@
-import { addComment } from "@/service/posts";
+import { addComment, deleteComment } from "@/service/posts";
 import { withSessionUser } from "@/util/session";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -10,8 +10,22 @@ export async function POST(req: NextRequest) {
       return new Response("Bad Request", { status: 400 });
     }
 
-    return addComment(id, user.id, comment) //
+    return addComment(id, user.id, comment)
       .then((res) => NextResponse.json(res))
+      .catch((error) => new Response(JSON.stringify(error), { status: 500 }));
+  });
+}
+
+export async function DELETE(req: NextRequest) {
+  return withSessionUser(async (user) => {
+    const { id, userId } = await req.json();
+
+    if (!id || !userId) {
+      return new Response("Bad Request", { status: 400 });
+    }
+
+    return deleteComment(id, user.id)
+      .then(() => NextResponse.json({ success: true }))
       .catch((error) => new Response(JSON.stringify(error), { status: 500 }));
   });
 }
